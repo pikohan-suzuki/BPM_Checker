a0ea2334d887cccca2abe4678927238af19d4512
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-12 23:58:33 +0900

    テスト用の処理を追加

--- BPM_Checker/app/src/main/java/com/amebaownd/pikohan_nwiatori/bpm_checker/MainActivity.kt
+++ BPM_Checker/app/src/main/java/com/amebaownd/pikohan_nwiatori/bpm_checker/MainActivity.kt
@@ -2,11 +2,32 @@ package com.amebaownd.pikohan_nwiatori.bpm_checker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.amebaownd.pikohan_nwiatori.bpm_checker.View.BarGraph
+import com.amebaownd.pikohan_nwiatori.bpm_checker.View.Circle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var barGraph:BarGraph
+    lateinit var circle:Circle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        barGraph = findViewById<com.amebaownd.pikohan_nwiatori.bpm_checker.View.BarGraph>(R.id.bar_graph)
+        circle=findViewById(R.id.circle)
+
+        val rand = Random()
+
+        findViewById<Button>(R.id.button).setOnClickListener{barGraph.addBar(rand.nextInt(100).toFloat())
+        circle.update()}
+    }
+
+    override fun onResume() {
+        super.onResume()
+        barGraph.addBar(10f)
+
     }
 }
