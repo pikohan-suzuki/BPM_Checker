81dcf804a771c7347608f3f480c1fa1461365233
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-13 19:01:37 +0900

    半円のボタン

---
+++ BPM_Checker/app/src/main/java/com/amebaownd/pikohan_nwiatori/bpm_checker/Drawable/HalfCircleButton.kt
@@ -0,0 +1,39 @@
+package com.amebaownd.pikohan_nwiatori.bpm_checker.Drawable
+
+import android.graphics.Canvas
+import android.graphics.ColorFilter
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.drawable.Drawable
+
+class HalfCircleButton(private val color:Int,private val direction:Direction) : Drawable() {
+    private var paint = Paint()
+
+    override fun draw(canvas: Canvas) {
+        paint.color=color
+        paint.isAntiAlias=true
+        val rectF= RectF(bounds)
+        if(direction==Direction.LEFT) {
+            canvas.translate(bounds.right/2f,0f)
+            canvas.drawArc(rectF, 90f, 180f, true, paint)
+        }else if(direction==Direction.RIGHT){
+            canvas.translate(-bounds.right/2f,0f)
+            canvas.drawArc(rectF, -90f, 180f, true, paint)
+        }else if(direction==Direction.BOTTOM){
+            canvas.translate(0f,bounds.bottom/2f)
+            canvas.drawArc(rectF,0f, 180f, true, paint)
+        }else{
+            canvas.translate(0f,-bounds.bottom/2f)
+            canvas.drawArc(rectF, -180f, 180f, true, paint)
+        }
+    }
+
+    override fun setAlpha(alpha: Int) {
+        paint.alpha = alpha
+    }
+    override fun getOpacity() = paint.alpha
+
+    override fun setColorFilter(colorFilter: ColorFilter) {
+        paint.colorFilter = colorFilter
+    }
+}
