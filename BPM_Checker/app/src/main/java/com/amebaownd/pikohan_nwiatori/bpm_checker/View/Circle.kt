10563e63667e2e96c00dfd1330592c7fa04b681b
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-13 18:50:16 +0900

    アニメーションを追加

--- BPM_Checker/app/src/main/java/com/amebaownd/pikohan_nwiatori/bpm_checker/View/Circle.kt
+++ BPM_Checker/app/src/main/java/com/amebaownd/pikohan_nwiatori/bpm_checker/View/Circle.kt
@@ -1,5 +1,7 @@
 package com.amebaownd.pikohan_nwiatori.bpm_checker.View
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -10,63 +12,91 @@ import com.amebaownd.pikohan_nwiatori.bpm_checker.R
 
 class Circle : View {
     constructor(context: Context) : super(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         initView(attrs)
     }
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         initView(attrs)
     }
 
     private var centerx = 0f
     private var centery = 0f
-    private var radius = 100f
-    private var max=300f
-    private var color:Int=Color.RED
-    private var strokeWidth=0.5f
+    private var radius = 0f
+    private var previousRadius = 0f
+    private var previousAlpha =0f
+    private var max = 300f
+    private var color: Int = Color.RED
+    private var strokeWidth = 0.5f
     private var primaryPaint = Paint()
-    private var secondaryPaint=Paint()
+    private var secondaryPaint = Paint()
 
-    fun setCenterx(num:Float){this.centerx=num}
-    fun setCentery(num:Float){this.centery=num}
-    fun setRaduis(num:Float){this.radius=num}
-    fun setMax(num:Float){this.max=num}
-    fun setStrokeWidth(width:Float){
-        this.strokeWidth=width
-        primaryPaint.strokeWidth=width
-        secondaryPaint.strokeWidth=0.5f
-    }
-    fun setColor(color:Int){
-        this.color=color
-        primaryPaint.color=color
-        secondaryPaint.color=color
+    fun setCenterx(num: Float) {
+        this.centerx = num
     }
 
-    private fun initView(attrs:AttributeSet?){
+    fun setCentery(num: Float) {
+        this.centery = num
+    }
+
+    fun setRaduis(num: Float) {
+        this.radius = num
+    }
+
+    fun setMax(num: Float) {
+        this.max = num
+    }
+
+    fun setStrokeWidth(width: Float) {
+        this.strokeWidth = width
+        primaryPaint.strokeWidth = width
+        secondaryPaint.strokeWidth = 0.5f
+    }
+
+    fun setColor(color: Int) {
+        this.color = color
+        primaryPaint.color = color
+        secondaryPaint.color = color
+    }
+
+    private fun initView(attrs: AttributeSet?) {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.Circle)
         setColor(typedArray.getColor(R.styleable.Circle_circle_color, Color.RED))
         setStrokeWidth(typedArray.getFloat(R.styleable.Circle_stroke_width, 2f))
         setMax(typedArray.getFloat(R.styleable.Circle_circle_max, 300f))
 
-        setCenterx(width/2f)
-        setCentery(height/2f)
-        primaryPaint.style=Paint.Style.STROKE
+        setCenterx(width / 2f)
+        setCentery(height / 2f)
+        primaryPaint.style = Paint.Style.STROKE
     }
 
-    fun update(data:Float){
-        if(data>max)
-            radius=height/2f
-        else
-            radius=height/2*data/max
-        setCenterx(width/2f)
-        setCentery(height/2f)
+    fun update(data: Float) {
+        previousRadius = radius
+        if (data > max) {
+            radius = height / 2f
+            previousAlpha=secondaryPaint.alpha.toFloat()
+            secondaryPaint.alpha = 100
+        } else {
+            radius = height / 2 * data / max
+            previousAlpha=secondaryPaint.alpha.toFloat()
+            secondaryPaint.alpha = 200 - (100 * data / max).toInt()
+        }
+        setCenterx(width / 2f)
+        setCentery(height / 2f)
         invalidate()
     }
 
     override fun onDraw(canvas: Canvas?) {
         if (centerx != 0f) {
-            secondaryPaint.alpha=30
             canvas!!.drawCircle(centerx, centery, radius, primaryPaint)
-            canvas!!.drawCircle(centerx,centery,radius-strokeWidth,secondaryPaint)
+            canvas!!.drawCircle(centerx, centery, radius - strokeWidth, secondaryPaint)
+            val alphaAnimation = PropertyValuesHolder.ofFloat("alpha", previousAlpha/255f, secondaryPaint.alpha/255f)
+            val xScaleAnimation = PropertyValuesHolder.ofFloat("scaleX",previousRadius/radius,1f)
+            val yScaleAnimation = PropertyValuesHolder.ofFloat("scaleY",previousRadius/radius,1f)
+            val objectAnimator = ObjectAnimator.ofPropertyValuesHolder(this, alphaAnimation,xScaleAnimation,yScaleAnimation)
+            objectAnimator.setDuration(200)
+            objectAnimator.start()
         }
+
     }
 }
