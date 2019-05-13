9f7634cebb881c8c77c74a81df898a4f83dccd85
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-13 21:31:15 +0900

    resetとsaveの追加

--- BPM_Checker/app/src/main/java/com/amebaownd/pikohan_nwiatori/bpm_checker/MainActivity.kt
+++ BPM_Checker/app/src/main/java/com/amebaownd/pikohan_nwiatori/bpm_checker/MainActivity.kt
@@ -5,7 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.amebaownd.pikohan_nwiatori.bpm_checker.Drawable.Direction
-import com.amebaownd.pikohan_nwiatori.bpm_checker.Drawable.HalfCircle
+import com.amebaownd.pikohan_nwiatori.bpm_checker.Drawable.HalfCircleButton
+import com.amebaownd.pikohan_nwiatori.bpm_checker.Drawable.TabButton
 import com.amebaownd.pikohan_nwiatori.bpm_checker.View.BarGraph
 import com.amebaownd.pikohan_nwiatori.bpm_checker.View.Circle
 
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private var previousTime:Long=0
     private var sum =0L
     private var count=0
+    private var isSaved=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +25,38 @@ class MainActivity : AppCompatActivity() {
 
         barGraph = findViewById(R.id.bar_graph)
         circle = findViewById(R.id.circle)
-        val button = findViewById<Button>(R.id.button)
-        button.background=HalfCircle(Color.YELLOW, Direction.BOTTOM)
+        val recordButton = findViewById<Button>(R.id.record_button)
+        recordButton.background=HalfCircleButton(Color.YELLOW, Direction.TOP)
+        val saveButton = findViewById<Button>(R.id.save)
+        saveButton.background=TabButton(Color.YELLOW,Direction.LEFT)
+        saveButton.setOnClickListener{
+            isSaved=true
+            saveButton.text=getString(R.string.saved)
+        }
+        val resetButton = findViewById<Button>(R.id.reset)
+        resetButton.background=TabButton(Color.YELLOW,Direction.LEFT)
+        resetButton.setOnClickListener{
+            barGraph.reset()
+            circle.reset()
+            previousTime=0L
+            sum=0L
+            count=0
+            isSaved=false
+            saveButton.text=getString(R.string.save)
+        }
 
-        findViewById<Button>(R.id.button).setOnClickListener {
-            if(previousTime==0L)
-                previousTime=System.currentTimeMillis()
-            else{
-                count+=1
-                val time = System.currentTimeMillis()
-                sum+=time-previousTime
-                barGraph.addBar((time-previousTime).toFloat())
-                circle.update((60000/(sum/count)).toFloat())
-                previousTime=time
+        findViewById<Button>(R.id.record_button).setOnClickListener {
+            if(!isSaved) {
+                if (previousTime == 0L)
+                    previousTime = System.currentTimeMillis()
+                else {
+                    count += 1
+                    val time = System.currentTimeMillis()
+                    sum += time - previousTime
+                    barGraph.addBar((time - previousTime).toFloat())
+                    circle.update((60000 / (sum / count)).toFloat())
+                    previousTime = time
+                }
             }
         }
     }
